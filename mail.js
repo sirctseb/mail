53a60069a6af4fbb8898b27450dc75c902491026
Christopher Best <bestchris@gmail.com>
2015-07-29 17:19:09 -0400

    lookup

--- mail.js
+++ mail.js
@@ -6,24 +6,29 @@ var mail = {
 	// TODO figure out how to use backspace without navigating back (should be 8)
 	deleteCode: 59,
 	enterCode: 13,
-	cancelCode: 27
+	cancelCode: 27,
+	zipAPIKey: 'js-9KQAgn5a4bqHxzAYivYpN2WYoJlZCjgM47mlurvzDWgbvRaOn8dba9azTvzwOPiT',
+	zipCache: {},
+	apiRequest: null
 };
 
 var boot = function() {
 	// handle visible character typing
 	document.onkeypress = function(event) {
-		console.log(event.which);
 		if (mail.input.length < 5 && event.which >= 48 && event.which <= 58) {
 			mail.input.push(event.which - 48);
 			updateDisplay();
 		} else if (mail.input.length > 0 && event.which === mail.deleteCode) {
 			mail.input.pop();
 			updateDisplay();
-		}
+	 	} else if (event.which === 97) {
+	 		lookupZipViaAPI(mail.input.join(''), function(response) {
+				document.querySelector('#lookup-display').textContent = response.join(', ');
+	 		});
+	 	}
 	};
 
 	document.onkeydown = function(event) {
-		console.log(event.which);
 		if (event.which === mail.enterCode && mail.input.length > 1) {
 			// TODO feedback that letter was sent to box
 			mail.input = [];
@@ -35,22 +40,64 @@ var boot = function() {
 	};
 };
 
+// checks cache for zip entry and looks up via API if missed
+var lookupZip = function(zip, onLookup) {
+	if (zip.toString() in mail.zipCache) {
+		onLookup(mail.zipCache[zip.toString()]);
+	} else {
+		// check via api
+		var suffixes = ['01', '02', '03'];
+		var result = lookupZipViaAPI(zip.join('') + '98', function(result) {
+			mail.zipCache[zip.toString()] = result;
+			onLookup(result);
+		});
+	}
+};
+
+var lookupZipViaAPI = function(zip, onLookup) {
+	// abort another request that may be in process
+	if (mail.apiRequest) {
+		mail.apiRequest.abort();
+	}
+
+	var request = new XMLHttpRequest();
+	mail.apiRequest = request;
+	request.open('GET', 'https://www.zipcodeapi.com/rest/' + mail.zipAPIKey
+		+ '/info.json/' + zip + '/radians');
+
+	request.onreadystatechange = function() {
+		if (request.readyState === 4) {
+			// TODO error handling
+			if (request.status === 200) {
+				var obj = JSON.parse(request.responseText);
+				onLookup([obj['city'], obj['state']]);
+			} else if (request.status === 400) {
+				onLookup(null);
+			}
+			mail.apiRequest = null;
+		}
+	};
+
+	request.send();
+}
+
 var updateDisplay = function() {
 	// set input display
 	document.querySelector('#input-value').textContent = mail.input.join('');
 
-	console.log(document.querySelector('#placeholder').classList);
 	// show address lookup
 	if (mail.input.length === 2) {
 		document.querySelector('#placeholder').classList.remove('hidden');
 		document.querySelector('#lookup-display').textContent = 'Detroit, MI';
-	} else if (mail.input.length > 2) {
-		// TODO assumes it will have no other classes
+	} else if (mail.input.length === 3) {
 		document.querySelector('#placeholder').classList.add('hidden');
-		// TODO perform zip lookup and display
-		document.querySelector('#lookup-display').textContent = 'Other city, state';
+		lookupZip(mail.input, function(result) {
+			console.log(result);
+			document.querySelector('#lookup-display').textContent = result.join(', ');
+		})
+	} if (mail.input.length > 3) {
+		// TODO anything on 4,5?
 	} else {
-		// TODO assumes it will have no other classes
 		document.querySelector('#placeholder').classList.add('hidden');
 		document.querySelector('#lookup-display').textContent = '';
 	}
